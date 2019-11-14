766163333f3b75d344370c3655ada2e24e89f7ac
lindaring91 <linda.simelane@proagrica.com>
2019-11-14 16:52:36 +0200

    Basic auth but failing

--- src/main/java/com/lindaring/security/service/UserService.java
+++ src/main/java/com/lindaring/security/service/UserService.java
@@ -1,20 +1,32 @@
 package com.lindaring.security.service;
 
 import com.lindaring.security.entity.User;
+import com.lindaring.security.model.UserCredentials;
 import com.lindaring.security.repo.UserRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
   @Autowired
   private UserRepo userRepo;
 
-  public User getUser(String username) {
-    Optional<User> user = userRepo.findUserByUsername(username);
-    return user.orElse(null);
+  @Override
+  public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+    Optional<User> user = userRepo.findUserByUsername(userName);
+
+    user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+    UserCredentials userCredentials = user.map(UserCredentials::new).get();
+    log.info("User: " + userCredentials);
+    return userCredentials;
   }
 }
